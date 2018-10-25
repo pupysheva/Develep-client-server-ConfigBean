a00d6964b393e531c0e34b3b1090c1b16ef01fd0
pupysheva <anime-manga@bk.ru>
2018-10-25 11:25:11 +0300

    PostProcessor dataInjector init

--- src/main/java/ru/mirea/DateInjectorBeanPostProcessor.java
+++ src/main/java/ru/mirea/DateInjectorBeanPostProcessor.java
@@ -3,10 +3,34 @@ package ru.mirea;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
+import java.lang.reflect.Field;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DateInjectorBeanPostProcessor implements BeanPostProcessor {
-    
+
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
         System.out.println("BEAN NAME: " + beanName);
+
+        for(Field field : bean.getClass().getDeclaredFields()) // get date and fullDate and more private and public filds.
+        {
+            // a lot of cost of CPU
+            DateInject dateInject = field.getAnnotation(DateInject.class);
+            if(dateInject != null) {
+                String format = dateInject.format();
+                Date date = new Date();
+                SimpleDateFormat sdf = new SimpleDateFormat(format);
+                String formattedDate = sdf.format(date); // threads-safe
+                // sdf.parse(); threads-no-safe
+                field.setAccessible(true); // get access for write.
+                try {
+                    field.set(bean, formattedDate);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
         return bean;
     }
 
